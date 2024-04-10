c1f7a7391f3428eb81bae1a1bfc79d4bf6874d5d
Milan Dzenovljanovic <miland@ketryx.com>
2024-04-10 22:26:36 +0200

    Add ts stub file

---
+++ sw.ts
@@ -0,0 +1,36 @@
+/**
+ * Some description. And some more details. @itemImplements:req-cucumber-files
+ * Additional details. @itemHasParent:"sw-sync-with-jira" @itemAffects:sw-cucumber-parser
+ *
+ * @itemId:TS1
+ * @itemFullfills:"req-managing-items, req-cucumber-files"
+ *
+ * @param captures The captures to extract named captures from.
+ * @param name The name of the named capture to extract.
+ */
+function getNamedCapture(captures: any[], name: string) {
+    for (const capture of captures) {
+        if (capture.name === name) {
+            return capture.node.text;
+        }
+    }
+    return undefined;
+}
+
+/**
+ * Some description. And some more details. @itemImplements:req-cucumber-files
+ * Additional details. @itemHasParent:"ABC-456, sw-cucumber-parser" @itemAffects:req-cucumber-files
+ *
+ * @itemFullfills:"req-cucumber-files"
+ *
+ * @param captures The captures to extract named captures from.
+ * @param name The name of the named capture to extract.
+ */
+function getNamedFormats(captures: any[], name: string) {
+    for (const capture of captures) {
+        if (capture.name === name) {
+            return capture.node.text;
+        }
+    }
+    return undefined;
+}
